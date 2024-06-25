c2233252d72d48946bd4e69888eb7d1233d8ea33
AnaOliari <carolina97627687@gmail.com>
2024-06-24 22:45:52 -0300

    Update App.tsx

--- front-tarefas/src/App.tsx
+++ front-tarefas/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ListarTarefas from "./pages/ListarTarefas";
 import CadastrarTarefa from "./pages/CadastrarTarefas";
 import AlterarTarefa from "./pages/AlterarTarefa";
